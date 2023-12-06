fac375df9ecd83f429cb9747088b6401b2833d2a
nathan ks <nathanks0210@gmail.com>
2023-12-05 22:59:34 -0300

    refactor: server up text

--- src/shared/server/index.ts
+++ src/shared/server/index.ts
@@ -2,4 +2,6 @@ import { app } from './app';
 
 const port = process.env.PORT || 3333;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () =>
+  console.log(` Server is running on port:${port} 📚🚀🚀🚀 `),
+);
