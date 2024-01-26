be86e14d69efd9094115030786b88ec6eba11bba
nathan ks <nathanks0210@gmail.com>
2024-01-25 21:13:54 -0300

    feat: user DTOS

---
+++ src/modules/User/DTO/IUpdateUserDTO.ts
@@ -0,0 +1,33 @@
+class IUpdateUserDTO {
+  usu_Id: string;
+
+  usu_Nome?: string;
+
+  usu_Email?: string;
+
+  usu_Senha?: string;
+
+  usu_Telefone?: string;
+
+  usu_CPF?: string;
+
+  usu_Nasc?: Date;
+
+  usu_avatar?: string;
+
+  usu_Rua?: string;
+
+  usu_Numero?: string;
+
+  usu_Bairro?: string;
+
+  usu_CEP?: string;
+
+  usu_Complemento?: string;
+
+  usu_est_id?: string;
+
+  usu_cid_id?: string;
+}
+
+export { IUpdateUserDTO };
