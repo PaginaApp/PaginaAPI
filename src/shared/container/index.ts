38b82f48498666a152186d55a42018b60acc9688
nathan ks <nathanks0210@gmail.com>
2024-01-25 21:12:42 -0300

    feat: container injection

---
+++ src/shared/container/index.ts
@@ -0,0 +1,9 @@
+import { UserRepository } from '@modules/User/repository/UserRepository';
+import { IUserRepository } from '@modules/User/repository/UserRepository.interface';
+import { container } from 'tsyringe';
+import { HashProvider } from './providers/hashProvider/implementation/HashProvider';
+import { IHashProvider } from './providers/hashProvider/model/IHashProvider';
+
+container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
+
+container.registerSingleton<IHashProvider>('HashProvider', HashProvider);
