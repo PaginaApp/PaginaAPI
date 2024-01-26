edf63218cb661557630438c90871006a4ca91ffa
nathan ks <nathanks0210@gmail.com>
2024-01-25 21:14:04 -0300

    feat: user controller

---
+++ src/modules/User/controller/User.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { CreateUser } from '../service/CreateUser.service';
+
+class UserController {
+  async create(req: Request, res: Response) {
+    const { usu_Nome, usu_Email, usu_Senha, usu_Telefone, usu_CPF, usu_Nasc } =
+      req.body;
+
+    const createUser = container.resolve(CreateUser);
+
+    const user = await createUser.execute({
+      usu_Nome,
+      usu_Email,
+      usu_Senha,
+      usu_Telefone,
+      usu_CPF,
+      usu_Nasc,
+      usu_pap_id: undefined,
+    });
+
+    return res.status(201).json(user);
+  }
+}
+
+export { UserController };
