860e2b6a25f70712cbb9c3388533a281ba75b7f7
nathan ks <nathanks0210@gmail.com>
2024-01-31 18:37:35 -0300

    feat: list user on controller

--- src/modules/User/controller/User.controller.ts
+++ src/modules/User/controller/User.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { User } from '../entity/User';
 import { CreateUser } from '../service/CreateUser.service';
 import { DeleteUserService } from '../service/DeleteUser.service';
 import { FindById } from '../service/FindById.service';
+import { ListUserService } from '../service/ListUser.service';
 
 class UserController {
   async create(req: Request, res: Response) {
@@ -42,6 +44,20 @@ class UserController {
     await deleteUser.execute(id);
 
     return res.status(204).send();
+  }
+
+  async ListUser(req: Request, res: Response) {
+    const { page, limit, filter } = req.query;
+
+    const listUserService = container.resolve(ListUserService);
+
+    const users = await listUserService.execute({
+      page: Number(page),
+      limit: Number(limit),
+      filter: filter as Partial<User>,
+    });
+
+    return res.status(200).json(users);
   }
 }
 
