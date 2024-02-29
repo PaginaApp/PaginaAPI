8e747f6d5670c7999e3eb576a83652632eeb2fd2
nathan ks <nathanks0210@gmail.com>
2024-02-28 23:30:22 -0300

    feat: papel route on server routes

--- src/shared/server/routes.ts
+++ src/shared/server/routes.ts
@@ -1,9 +1,12 @@
+import { papelRoutes } from '@modules/Papel/routes/Papel.routes';
 import { userRouter } from '@modules/User/routes/User.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/users', userRouter);
+
+routes.use('/papel', papelRoutes);
 
 routes.get('/', (request, response) => {
   response.send('Bem Vindo ao API rest da plataforma Página 📚🚀🚀🚀');
