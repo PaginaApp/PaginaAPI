88894d501c239ca241ffd221955ea7dbbaeca3d3
nathan ks <nathanks0210@gmail.com>
2024-02-28 23:29:39 -0300

    feat: papel conteiner injection

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,3 +1,5 @@
+import { IPapelRepository } from '@modules/Papel/repository/IPapelRepository.interface';
+import { PapelRepository } from '@modules/Papel/repository/PapelRepository';
 import { UserRepository } from '@modules/User/repository/UserRepository';
 import { IUserRepository } from '@modules/User/repository/UserRepository.interface';
 import { container } from 'tsyringe';
@@ -5,5 +7,10 @@ import { HashProvider } from './providers/hashProvider/implementation/HashProvid
 import { IHashProvider } from './providers/hashProvider/model/IHashProvider';
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
+
+container.registerSingleton<IPapelRepository>(
+  'PapelRepository',
+  PapelRepository,
+);
 
 container.registerSingleton<IHashProvider>('HashProvider', HashProvider);
