c622b24310a90e5fe208cfb183ff03caa37431d6
nathan ks <nathanks0210@gmail.com>
2024-03-08 21:44:55 -0300

    feat: remove useless code

--- src/shared/config/auth.ts
+++
@@ -1,9 +0,0 @@
-export const jwtConfig = {
-  secret: process.env.API_SECRET,
-  expiresIn: process.env.API_EXPIRATION,
-};
-
-export const refreshTokenConfig = {
-  expiresIn: process.env.REFRESH_EXPIRATION,
-  prefix: process.env.REFRESH_PREFIX,
-};
