cb00745ab3d30ababb31e80d63d1872c11ebb4da
nathan ks <nathanks0210@gmail.com>
2024-03-20 23:44:39 -0300

    feat: endereco function on user repository

--- src/modules/User/repository/UserRepository.interface.ts
+++ src/modules/User/repository/UserRepository.interface.ts
@@ -1,9 +1,12 @@
+import { IEnderecoDTO } from '@modules/Endereco/DTO/IEnderecoDTO';
 import { IRepository } from '@shared/interfaces/Repository';
 import { IcreateUserDTO } from '../DTO/ICreateUserDTO';
 import { IUpdateUserDTO } from '../DTO/IUpdateUserDTO';
 import { User } from '../entity/User';
 
 interface IUserRepository
-  extends IRepository<User, IcreateUserDTO, IUpdateUserDTO> {}
+  extends IRepository<User, IcreateUserDTO, IUpdateUserDTO> {
+  endereco(data: IEnderecoDTO, usu_Id: string): Promise<IEnderecoDTO>;
+}
 
 export { IUserRepository };
