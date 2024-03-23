2848658abeb1675b8b04e18d592c86f437c7af9c
nathan ks <nathanks0210@gmail.com>
2024-03-23 18:29:04 -0300

    feat: editora repository

---
+++ src/modules/Editora/repository/EditoraRepository.ts
@@ -0,0 +1,39 @@
+import { prisma } from '@shared/database';
+import { IPaginatedRequest } from '@shared/interfaces/IPaginatedRequest';
+import { IPaginatedResponse } from '@shared/interfaces/IPaginatedResponse';
+import { Editora } from '../entitie/Editora';
+import { IEditoraRepository } from './IEditoraRepository.interface';
+
+class EditoraRepository implements IEditoraRepository {
+  async findBy(partial: Partial<Editora>): Promise<Editora | null> {
+    const editora = await prisma.editora.findFirst({
+      where: partial,
+    });
+
+    return editora;
+  }
+
+  async listBy({
+    page = 1,
+    limit = 10,
+    filter,
+  }: IPaginatedRequest<Editora>): Promise<IPaginatedResponse<Editora>> {
+    const data = await prisma.editora.findMany({
+      where: filter,
+      skip: (page - 1) * limit,
+      take: limit,
+    });
+
+    const total = await prisma.editora.count({
+      where: filter,
+    });
+
+    return {
+      results: data,
+      total,
+      page,
+      limit,
+    };
+  }
+}
+export { EditoraRepository };
---
+++ src/modules/Editora/repository/IEditoraRepository.interface.ts
@@ -0,0 +1,6 @@
+import { IBasicRepository } from '@shared/interfaces/BasicRepository';
+import { Editora } from '../entitie/Editora';
+
+interface IEditoraRepository extends IBasicRepository<Editora> {}
+
+export { IEditoraRepository };
