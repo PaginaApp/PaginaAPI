0b6c636873dfb3817d6493898052618aaf78108d
nathan ks <nathanks0210@gmail.com>
2024-03-26 20:38:50 -0300

    feat: livro routes on app routes

--- src/shared/server/routes.ts
+++ src/shared/server/routes.ts
@@ -1,3 +1,4 @@
+import { livroRoutes } from '@modules/Livro/routes/Livro.routes';
 import { papelRoutes } from '@modules/Papel/routes/Papel.routes';
 import { sessionRouter } from '@modules/User/routes/Session.routes';
 import { userRouter } from '@modules/User/routes/User.routes';
@@ -10,6 +11,8 @@ routes.use('/users', userRouter);
 routes.use('/papel', papelRoutes);
 
 routes.use('/session', sessionRouter);
+
+routes.use('/livro', livroRoutes);
 
 routes.get('/', (request, response) => {
   response.send('Bem Vindo ao API rest da plataforma Página 📚🚀🚀🚀');
