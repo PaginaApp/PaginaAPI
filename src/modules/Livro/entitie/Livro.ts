c9b9be5c83e3c139d2134e6110fd0c391b041e69
nathan ks <nathanks0210@gmail.com>
2024-03-26 20:36:25 -0300

    feat: livro entitie

---
+++ src/modules/Livro/entitie/Livro.ts
@@ -0,0 +1,17 @@
+class Livro {
+  liv_Id: string;
+
+  liv_Titulo: string;
+
+  liv_Ano: string;
+
+  liv_Sinopse: string;
+
+  liv_ISBN: string;
+
+  liv_CriadoEm: Date;
+
+  liv_AtualizadoEm: Date;
+}
+
+export default Livro;
