877cc6e51c2f471929d681e09e36b947f6e618e9
nathan ks <nathanks0210@gmail.com>
2024-04-10 20:20:02 -0300

    feat: create and update on livro controller

--- src/modules/Livro/controller/Livro.controller.ts
+++ src/modules/Livro/controller/Livro.controller.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import Livro from '../entitie/Livro';
+import { CreateLivroService } from '../service/CreateLivro.service';
+import { FindByISBNService } from '../service/FindByISBN.service';
 import { FindByNameService } from '../service/FindByName.service';
+import { ListByISBNService } from '../service/ListByISBN.service';
 import { ListByNameService } from '../service/ListByName.service';
+import { UpdateLivroService } from '../service/UpdateLivro.service';
 
 class LivroController {
   async findByName(req: Request, res: Response) {
@@ -18,7 +22,7 @@ class LivroController {
   async findByISBN(req: Request, res: Response) {
     const { liv_ISBN } = req.params;
 
-    const findByNameService = container.resolve(FindByNameService);
+    const findByNameService = container.resolve(FindByISBNService);
 
     const livro = await findByNameService.execute(liv_ISBN);
 
@@ -27,14 +31,14 @@ class LivroController {
 
   async listByName(req: Request, res: Response) {
     const { page, limit } = req.query;
-    const { liv_Titulo } = req.params;
+    const { liv_Titulo } = req.query;
 
     const listByNameService = container.resolve(ListByNameService);
 
     const livros = await listByNameService.execute({
       page: Number(page),
       limit: Number(limit),
-      filter: liv_Titulo as Partial<Livro>,
+      filter: { liv_Titulo } as Partial<Livro>,
     });
 
     return res.status(200).json(livros);
@@ -42,9 +46,9 @@ class LivroController {
 
   async listByISBN(req: Request, res: Response) {
     const { page, limit } = req.query;
-    const { liv_ISBN } = req.params;
+    const { liv_ISBN } = req.body;
 
-    const listByNameService = container.resolve(ListByNameService);
+    const listByNameService = container.resolve(ListByISBNService);
 
     const livros = await listByNameService.execute({
       page: Number(page),
@@ -53,6 +57,57 @@ class LivroController {
     });
 
     return res.status(200).json(livros);
+  }
+
+  async createLivro(req: Request, res: Response) {
+    const {
+      liv_Titulo,
+      liv_Ano,
+      liv_Sinopse,
+      liv_ISBN,
+      liv_aut_id,
+      liv_edi_id,
+    } = req.body;
+
+    const createLivroService = container.resolve(CreateLivroService);
+
+    const data = await createLivroService.execute({
+      liv_Titulo,
+      liv_Ano,
+      liv_Sinopse,
+      liv_ISBN,
+      liv_aut_id,
+      liv_edi_id,
+    });
+
+    return res.status(200).json(data);
+  }
+
+  async updateLivro(req: Request, res: Response) {
+    const {
+      liv_Titulo,
+      liv_Ano,
+      liv_Sinopse,
+      liv_ISBN,
+      liv_aut_id,
+      liv_edi_id,
+    } = req.body;
+
+    const { liv_Id } = req.params;
+
+    const updateLivroService = container.resolve(UpdateLivroService);
+
+    const data = await updateLivroService.execute({
+      liv_Titulo,
+      liv_Ano,
+      liv_Sinopse,
+      liv_ISBN,
+      liv_aut_id,
+      liv_edi_id,
+      liv_Id,
+    });
+
+    return res.status(200).json(data);
   }
 }
 
