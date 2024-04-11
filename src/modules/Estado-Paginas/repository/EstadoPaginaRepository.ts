bb0928810b04d2d13ebfca4ec569de6263734f13
nathan ks <nathanks0210@gmail.com>
2024-04-10 22:19:38 -0300

    feat: estado capa repository

---
+++ src/modules/Estado-Paginas/repository/EstadoPaginaRepository.ts
@@ -0,0 +1,42 @@
+import { prisma } from '@shared/database';
+import { IPaginatedRequest } from '@shared/interfaces/IPaginatedRequest';
+import { IPaginatedResponse } from '@shared/interfaces/IPaginatedResponse';
+import { EstadoPaginas } from '../entitie/EstadoPaginas';
+import { IEstadoPaginaRepository } from './IEstadoPaginaRepository.interface';
+
+class EstadoPaginaRepository implements IEstadoPaginaRepository {
+  async findBy(partial: Partial<EstadoPaginas>): Promise<EstadoPaginas | null> {
+    const data = await prisma.estado_Paginas.findFirst({
+      where: partial,
+    });
+
+    return data;
+  }
+
+  async listBy({
+    filter,
+    limit = 10,
+    page = 1,
+  }: IPaginatedRequest<EstadoPaginas>): Promise<
+    IPaginatedResponse<EstadoPaginas>
+  > {
+    const data = await prisma.estado_Paginas.findMany({
+      where: filter,
+      take: limit,
+      skip: (page - 1) * limit,
+    });
+
+    const total = await prisma.estado_Paginas.count({
+      where: filter,
+    });
+
+    return {
+      results: data,
+      total,
+      page,
+      limit,
+    };
+  }
+}
+
+export { EstadoPaginaRepository };
---
+++ src/modules/Estado-Paginas/repository/IEstadoPaginaRepository.interface.ts
@@ -0,0 +1,6 @@
+import { IBasicRepository } from '@shared/interfaces/BasicRepository';
+import { EstadoPaginas } from '../entitie/EstadoPaginas';
+
+interface IEstadoPaginaRepository extends IBasicRepository<EstadoPaginas> {}
+
+export { IEstadoPaginaRepository };
