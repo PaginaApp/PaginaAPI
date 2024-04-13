821625991478c3287cf62c62696d0a6b47386555
nathan ks <nathanks0210@gmail.com>
2024-04-12 22:10:43 -0300

    feat: exemplar entitie

---
+++ src/modules/Exemplar/entitie/Exemplar.ts
@@ -0,0 +1,21 @@
+class Exemplar {
+  exe_Id: string;
+
+  exe_Descricao: string;
+
+  exe_Negociando: boolean;
+
+  exe_liv_id: string;
+
+  exe_usu_id: string;
+
+  exe_epg_id: string;
+
+  exe_ecp_id: string;
+
+  exe_CriadoEm: Date;
+
+  exe_AtualizadoEm: Date;
+}
+
+export { Exemplar };
