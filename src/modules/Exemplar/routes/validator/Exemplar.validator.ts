5a7b2d81720e81089b6c6aff75d2f6ea9db97506
nathan ks <nathanks0210@gmail.com>
2024-04-12 22:13:30 -0300

    feat: exemplar routes and validator

---
+++ src/modules/Exemplar/routes/validator/Exemplar.validator.ts
@@ -0,0 +1,60 @@
+import { celebrate, Joi } from 'celebrate';
+
+export const createExemplarMiddleware = celebrate({
+  body: {
+    exe_Descricao: Joi.string().required(),
+    exe_Negociando: Joi.boolean().required(),
+    exe_liv_id: Joi.string().required().uuid(),
+    exe_usu_id: Joi.string().required().uuid(),
+    exe_epg_id: Joi.string().required().uuid(),
+    exe_ecp_id: Joi.string().required().uuid(),
+  },
+});
+
+export const updateExemplarMiddleware = celebrate({
+  params: {
+    exe_Id: Joi.string().uuid().required().uuid(),
+  },
+  body: {
+    exe_Descricao: Joi.string().required(),
+    exe_Negociando: Joi.boolean().required(),
+    exe_liv_id: Joi.string().required().uuid(),
+    exe_usu_id: Joi.string().required().uuid(),
+    exe_epg_id: Joi.string().required().uuid(),
+    exe_ecp_id: Joi.string().required().uuid(),
+  },
+});
+
+export const deleteExemplarMiddleware = celebrate({
+  params: {
+    exe_Id: Joi.string().uuid().required().uuid(),
+  },
+});
+
+export const listExemplarByUserMiddleware = celebrate({
+  query: {
+    page: Joi.number().required(),
+    limit: Joi.number().required(),
+  },
+
+  params: {
+    exe_usu_id: Joi.string().required().uuid(),
+  },
+});
+
+export const listExemplarByLivroMiddleware = celebrate({
+  query: {
+    page: Joi.number().required(),
+    limit: Joi.number().required(),
+  },
+
+  params: {
+    exe_liv_id: Joi.string().required().uuid(),
+  },
+});
+
+export const findExemplarMiddleware = celebrate({
+  params: {
+    exe_Id: Joi.string().required().uuid(),
+  },
+});
