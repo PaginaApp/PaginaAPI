1190ec0c0a18f3d911297c49a303e1bc06514637
nathan ks <nathanks0210@gmail.com>
2024-04-12 22:13:07 -0300

    feat: exemplar controller

---
+++ src/modules/Exemplar/controller/Exemplar.controller.ts
@@ -0,0 +1,118 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { Exemplar } from '../entitie/Exemplar';
+import { CreateExemplarService } from '../service/CreateExemplar.service';
+import { DeleteExemplarService } from '../service/DeleteExemplar.service';
+import { FindExemplarByIdService } from '../service/FindExemplarById.service';
+import { ListExemplarByLivroService } from '../service/ListaExemplarByLivro.service';
+import { ListExemplarByUserService } from '../service/ListExemparByUser.service';
+import { UpdateExemplarService } from '../service/UpdateExemplar.service';
+
+class ExemplarController {
+  async create(req: Request, res: Response) {
+    const {
+      exe_Descricao,
+      exe_Negociando = false,
+      exe_liv_id,
+      exe_usu_id,
+      exe_epg_id,
+      exe_ecp_id,
+    } = req.body;
+
+    const createExemplarService = container.resolve(CreateExemplarService);
+
+    const data = await createExemplarService.execute({
+      exe_Descricao,
+      exe_Negociando,
+      exe_liv_id,
+      exe_usu_id,
+      exe_epg_id,
+      exe_ecp_id,
+    });
+
+    return res.status(200).json(data);
+  }
+
+  async update(req: Request, res: Response) {
+    const {
+      exe_Descricao,
+      exe_Negociando,
+      exe_liv_id,
+      exe_usu_id,
+      exe_epg_id,
+      exe_ecp_id,
+    } = req.body;
+
+    const { exe_Id } = req.params;
+
+    const updateExemplarService = container.resolve(UpdateExemplarService);
+
+    const data = await updateExemplarService.execute({
+      exe_Descricao,
+      exe_Negociando,
+      exe_liv_id,
+      exe_usu_id,
+      exe_epg_id,
+      exe_ecp_id,
+      exe_Id,
+    });
+
+    return res.status(200).json(data);
+  }
+
+  async delete(req: Request, res: Response) {
+    const { exe_Id } = req.params;
+
+    const deleteExemplarService = container.resolve(DeleteExemplarService);
+
+    await deleteExemplarService.execute(exe_Id);
+
+    return res.status(200).json({ message: 'Exemplar deletado com sucesso' });
+  }
+
+  async listExemplarByUser(req: Request, res: Response) {
+    const { exe_usu_id } = req.params;
+    const { page, limit } = req.query;
+
+    const listExemplarByUserService = container.resolve(
+      ListExemplarByUserService,
+    );
+
+    const data = await listExemplarByUserService.execute({
+      page: Number(page),
+      limit: Number(limit),
+      filter: { exe_usu_id } as Partial<Exemplar>,
+    });
+
+    return res.status(200).json(data);
+  }
+
+  async listExemplarByLivro(req: Request, res: Response) {
+    const { exe_liv_id } = req.params;
+    const { page, limit } = req.query;
+
+    const listExemplarByBookService = container.resolve(
+      ListExemplarByLivroService,
+    );
+
+    const data = await listExemplarByBookService.execute({
+      page: Number(page),
+      limit: Number(limit),
+      filter: { exe_liv_id } as Partial<Exemplar>,
+    });
+
+    return res.status(200).json(data);
+  }
+
+  async findExemplar(req: Request, res: Response) {
+    const { exe_Id } = req.params;
+
+    const findExemplarByIdService = container.resolve(FindExemplarByIdService);
+
+    const exemplar = await findExemplarByIdService.execute(exe_Id);
+
+    return res.status(200).json(exemplar);
+  }
+}
+
+export { ExemplarController };
