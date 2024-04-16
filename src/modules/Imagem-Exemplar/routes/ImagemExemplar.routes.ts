93e56940e0043ab1092ceca9b7b240d73871096a
nathan ks <nathanks0210@gmail.com>
2024-04-16 20:39:30 -0300

    feat: imagem exemplar routes and validator

---
+++ src/modules/Imagem-Exemplar/routes/ImagemExemplar.routes.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import { ImagemExemplarController } from '../controller/ImagemExemplar.controller';
+import {
+  CreateImagemExemplarMidleware,
+  DeleteImagemExemplarMidleware,
+  GetImagemExemplarMidleware,
+} from './validator/ImagemExemplar.validator';
+
+const imagemExemplarRoutes = Router();
+
+const imagemExemplarController = new ImagemExemplarController();
+
+imagemExemplarRoutes.post(
+  '/:exe_Id/imagem-exemplar',
+  CreateImagemExemplarMidleware,
+  imagemExemplarController.create,
+);
+
+imagemExemplarRoutes.get(
+  '/imagem-exemplar/:iex_Id',
+  GetImagemExemplarMidleware,
+  imagemExemplarController.show,
+);
+
+imagemExemplarRoutes.delete(
+  '/imagem-exemplar/:iex_Id',
+  DeleteImagemExemplarMidleware,
+  imagemExemplarController.delete,
+);
+
+export { imagemExemplarRoutes };
