5b0cdc8ad874f9e0d869e7096c6eecbcfb1323e6
nathan ks <nathanks0210@gmail.com>
2024-04-20 20:48:31 -0300

    feat: Add Transacao routes on app routes

--- src/shared/server/routes.ts
+++ src/shared/server/routes.ts
@@ -5,6 +5,7 @@ import { exemplarRoutes } from '@modules/Exemplar/routes/Exemplar.routes';
 import { livroRoutes } from '@modules/Livro/routes/Livro.routes';
 import { papelRoutes } from '@modules/Papel/routes/Papel.routes';
 import { tipoTransacoesRoutes } from '@modules/Tipo-Transacao/routes/TipoTransacao.routes';
+import { transacaoRoutes } from '@modules/Transacao/routes/Transacao.routes';
 import { sessionRouter } from '@modules/User/routes/Session.routes';
 import { userRouter } from '@modules/User/routes/User.routes';
 import { Router } from 'express';
@@ -28,6 +29,8 @@ routes.use('/estadoPaginas', estadoPaginasRoutes);
 routes.use('/exemplar', exemplarRoutes);
 
 routes.use('/tipoTransacao', tipoTransacoesRoutes);
+
+routes.use('/transacao', transacaoRoutes);
 
 routes.get('/', (request, response) => {
   response.send('Bem Vindo ao API rest da plataforma Página 📚🚀🚀🚀');
