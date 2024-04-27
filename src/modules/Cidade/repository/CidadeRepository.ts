88a6a0e958cc174533b24a337f9e13320d55adea
nathan ks <nathanks0210@gmail.com>
2024-04-26 22:43:36 -0300

    feat: Add findByNameAndState method to CidadeRepository

--- src/modules/Cidade/repository/CidadeRepository.ts
+++ src/modules/Cidade/repository/CidadeRepository.ts
@@ -31,6 +31,23 @@ class CidadeRepository implements ICidadeRepository {
       limit,
     };
   }
+
+  findByNameAndState(
+    cid_Nome: string,
+    cid_est_id: string,
+  ): Promise<Cidade | null> {
+    const data = prisma.cidade.findFirst({
+      where: {
+        cid_Nome: {
+          equals: cid_Nome,
+          mode: 'insensitive',
+        },
+        cid_est_id,
+      },
+    });
+
+    return data;
+  }
 }
 
 export { CidadeRepository };
