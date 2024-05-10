61401012aac2d8a001027c7f1bd217fea12b5a73
nathan ks <nathanks0210@gmail.com>
2024-05-10 11:11:04 -0300

    feat: Add Autor routes and middleware for create, delete, and list operations

---
+++ src/modules/Autor/routes/Autor.routes.ts
@@ -0,0 +1,35 @@
+import verifyAutorization from '@shared/middlewares/VerifyAutorization';
+import { Router } from 'express';
+import { AutorController } from '../controller/Autor.controller';
+import {
+  createAutorMiddleware,
+  deleteAutorMiddleware,
+  listAutorMiddleware,
+} from './validator/Autor.validator';
+
+const autorRoutes = Router();
+
+const autorController = new AutorController();
+
+autorRoutes.post(
+  '/',
+  verifyAutorization([process.env.PAPEL_ADMINISTRADOR as string]),
+  createAutorMiddleware,
+  autorController.create,
+);
+
+autorRoutes.delete(
+  '/:aut_Id',
+  verifyAutorization([process.env.PAPEL_ADMINISTRADOR as string]),
+  deleteAutorMiddleware,
+  autorController.Delete,
+);
+
+autorRoutes.get(
+  '/',
+  verifyAutorization([process.env.PAPEL_ADMINISTRADOR as string]),
+  listAutorMiddleware,
+  autorController.list,
+);
+
+export { autorRoutes };
