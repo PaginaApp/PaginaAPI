2fb9844a37a8b1c3509c521f3fe9d104d2cc4e28
nathan ks <nathanks0210@gmail.com>
2024-05-09 21:57:40 -0300

    feat: create and delete on editora repository

--- src/modules/Editora/repository/IEditoraRepository.interface.ts
+++ src/modules/Editora/repository/IEditoraRepository.interface.ts
@@ -1,6 +1,10 @@
 import { IBasicRepository } from '@shared/interfaces/BasicRepository';
+import { CreateEditoraDTO } from '../DTO/CreateEditoraDTO';
 import { Editora } from '../entitie/Editora';
 
-interface IEditoraRepository extends IBasicRepository<Editora> {}
+interface IEditoraRepository extends IBasicRepository<Editora> {
+  create(entity: CreateEditoraDTO): Promise<Editora>;
+  delete(entity: Editora): Promise<void>;
+}
 
 export { IEditoraRepository };
