3ec6397b2ccdaf9c04cf57a98244ee7cddb824f8
nathan ks <nathanks0210@gmail.com>
2024-05-10 11:25:48 -0300

    feat: Add authorization middleware to Relatorio routes

--- src/modules/Relatorio/routes/Relatorio.routes.ts
+++ src/modules/Relatorio/routes/Relatorio.routes.ts
@@ -1,3 +1,4 @@
+import verifyAutorization from '@shared/middlewares/VerifyAutorization';
 import { Router } from 'express';
 import { RelatorioController } from '../controller/Relatorio.controller';
 import { getExemplaresPorMesMiddleware } from './Validator/Relatorio.validator';
@@ -8,20 +9,27 @@ const relatorioController = new RelatorioController();
 
 relatorioRoutes.get(
   '/exemplares/:ano',
+  verifyAutorization([process.env.PAPEL_ADMINISTRADOR as string]),
   getExemplaresPorMesMiddleware,
   relatorioController.getExemplaresPorMes,
 );
 
-relatorioRoutes.get('/users/idade', relatorioController.getUserByAge);
+relatorioRoutes.get(
+  '/users/idade',
+  verifyAutorization([process.env.PAPEL_ADMINISTRADOR as string]),
+  relatorioController.getUserByAge,
+);
 
 relatorioRoutes.get(
   '/transacoes/:ano',
+  verifyAutorization([process.env.PAPEL_ADMINISTRADOR as string]),
   getExemplaresPorMesMiddleware,
   relatorioController.getTransacaoPorMes,
 );
 
 relatorioRoutes.get(
   '/categorias/:ano',
+  verifyAutorization([process.env.PAPEL_ADMINISTRADOR as string]),
   getExemplaresPorMesMiddleware,
   relatorioController.getCategoriasCadastradasPorMes,
 );
