515503d9c653a56c3caae152ffc459da922587b0
nathan ks <nathanks0210@gmail.com>
2024-05-14 20:03:05 -0300

    feat: Add validation for `exe_trs_id`, `exe_Preco`, and `exe_Prazo` in CreateExemplarService

--- src/modules/Exemplar/service/CreateExemplar.service.ts
+++ src/modules/Exemplar/service/CreateExemplar.service.ts
@@ -1,6 +1,9 @@
 import { IEstadoCapaRepository } from '@modules/Estado-Capa/repository/IEstadoCapaRepository.interface';
 import { IEstadoPaginasRepository } from '@modules/Estado-Paginas/repository/IEstadoPaginaRepository.interface';
+import { ITipoTransacaoRepository } from '@modules/Tipo-Transacao/repository/TipoTransacaoRepository.interface';
+import { ITransacaoAceitaRepository } from '@modules/Transacao-Aceita/repository/TransacaoAceitaRepository.interface';
 import { IUserRepository } from '@modules/User/repository/UserRepository.interface';
+import { BadRequestError } from '@shared/errors/BadRequestError';
 import { EntityNotFoundError } from '@shared/errors/EntityNotFoundError';
 import { inject, injectable } from 'tsyringe';
 import { CreateExemplarDTO } from '../DTO/CreateExemplarDTO';
@@ -21,9 +24,18 @@ class CreateExemplarService {
 
     @inject('ExemplarRepository')
     private exemplarRepository: IExemplarRepository,
+
+    @inject('TipoTransacaoRepository')
+    private TipoTransacaoRepository: ITipoTransacaoRepository,
+
+    @inject('TransacaoAceitaRepository')
+    private transacaoAceitaRepository: ITransacaoAceitaRepository,
   ) {}
 
-  async execute(data: CreateExemplarDTO): Promise<Exemplar> {
+  async execute(
+    data: CreateExemplarDTO,
+    exe_trs_id: string[],
+  ): Promise<Exemplar> {
     const user = this.userRespository.findBy({
       usu_Id: data.exe_usu_id,
     });
@@ -52,7 +64,53 @@ class CreateExemplarService {
       );
     }
 
+    // verificação de tipos de transações aceitas
+    await Promise.all(
+      exe_trs_id.map(async (transacao) => {
+        const transacaoFind = await this.TipoTransacaoRepository.findBy({
+          ttr_Id: transacao,
+        });
+
+        if (!transacaoFind) {
+          throw new EntityNotFoundError('Tipo de transação não encontrado');
+        }
+
+        if (transacaoFind.ttr_Nome === 'Venda' && !data.exe_Preco) {
+          throw new BadRequestError('Preço esperado não enviado');
+        }
+
+        if (transacaoFind.ttr_Nome === 'Emprestimo' && !data.exe_Prazo) {
+          throw new BadRequestError('Prazo de empréstimo não informado');
+        }
+      }),
+    );
+
     const exemplar = await this.exemplarRepository.create(data);
+
+    // adiciona as transacoes aceitas ao exemplar
+
+    exe_trs_id.forEach(async (tipoTransacao) => {
+      await this.transacaoAceitaRepository.create({
+        exe_Id: exemplar.exe_Id,
+        ttr_Id: tipoTransacao,
+      });
+    });
+
+    const tiposTransacoesPromises = exe_trs_id.map(async (transacao) => {
+      const transacaoFind = await this.TipoTransacaoRepository.findBy({
+        ttr_Id: transacao,
+      });
+
+      if (!transacaoFind) {
+        throw new EntityNotFoundError('Tipo de transação não encontrado');
+      }
+
+      return transacaoFind;
+    });
+
+    const tiposTransacoes = await Promise.all(tiposTransacoesPromises);
+
+    Object.assign(exemplar, { tiposTransacoes });
 
     return exemplar;
   }
