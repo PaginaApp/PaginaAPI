69ee3d2f7ab2ad0f71d0be50c57d11ff25b55c88
nathan ks <nathanks0210@gmail.com>
2024-06-06 22:44:23 -0300

    feat: Add livro, categorias, and tiposTransacao to exemplar results

--- src/modules/Exemplar/service/ListExemparByUser.service.ts
+++ src/modules/Exemplar/service/ListExemparByUser.service.ts
@@ -1,7 +1,14 @@
+import { ICategoriaLivroRepository } from '@modules/Categoria-Livro/repository/ICategoria_LivroRepository.interface';
+import { Categoria } from '@modules/Categoria/entitie/Categoria';
+import { ICategoriaRepository } from '@modules/Categoria/repository/ICategoriaRepository.interface';
 import { IEstadoCapaRepository } from '@modules/Estado-Capa/repository/IEstadoCapaRepository.interface';
 import { IEstadoPaginasRepository } from '@modules/Estado-Paginas/repository/IEstadoPaginaRepository.interface';
 import { IExemplarTransacaoRepository } from '@modules/Exemplar-Transacao/repository/ExemplarTransacaoRepository.interface';
+import { ILivroRepository } from '@modules/Livro/repository/ILivroRepository.interface';
 import { IStatusTransacaoRepository } from '@modules/Status-Transacao/repository/StatusTransacaoRepository.interface';
+import { TipoTransacao } from '@modules/Tipo-Transacao/entitie/TipoTransacao';
+import { ITipoTransacaoRepository } from '@modules/Tipo-Transacao/repository/TipoTransacaoRepository.interface';
+import { ITransacaoAceitaRepository } from '@modules/Transacao-Aceita/repository/TransacaoAceitaRepository.interface';
 import { ITransacaoRepository } from '@modules/Transacao/repository/TransacaoRepository.interface';
 import { IUserRepository } from '@modules/User/repository/UserRepository.interface';
 import { EntityNotFoundError } from '@shared/errors/EntityNotFoundError';
@@ -34,6 +41,21 @@ class ListExemplarByUserService {
 
     @inject('StatusTransacaoRepository')
     private statusTransacaoRepository: IStatusTransacaoRepository,
+
+    @inject('LivroRepository')
+    private livroRepository: ILivroRepository,
+
+    @inject('CategoriaRepository')
+    private categoriaRepository: ICategoriaRepository,
+
+    @inject('CategoriaLivroRepository')
+    private categoriaLivroRepository: ICategoriaLivroRepository,
+
+    @inject('TipoTransacaoRepository')
+    private TipoTransacaoRepository: ITipoTransacaoRepository,
+
+    @inject('TransacaoAceitaRepository')
+    private transacaoAceitaRepository: ITransacaoAceitaRepository,
   ) {}
 
   async execute(
@@ -145,6 +167,67 @@ class ListExemplarByUserService {
         return Object.assign(exemplar, {
           estadoCapa: estadoCapaResult,
           estadoPaginas: estadoPaginasResult,
+        });
+      }),
+    );
+
+    await Promise.all(
+      exemplares.results.map(async (item) => {
+        const livroPromise = this.livroRepository.findBy({
+          liv_Id: item.exe_liv_id,
+        });
+
+        const [livro] = await Promise.all([livroPromise]);
+
+        const categoriaLivro = await this.categoriaLivroRepository.listBy({
+          filter: { liv_id: item.exe_liv_id },
+          page: 1,
+          limit: 3,
+        });
+
+        // para cada categoria, busca a categoria
+        const categorias: Categoria[] = [];
+        await Promise.all(
+          categoriaLivro.results.map(async (cat) => {
+            const categoria = await this.categoriaRepository.findBy({
+              cat_Id: cat.cat_id,
+            });
+
+            if (!categoria) {
+              return;
+            }
+
+            categorias.push(categoria);
+          }),
+        );
+
+        // tipos de transação
+        const transacaoAceita = await this.transacaoAceitaRepository.listBy({
+          filter: { exe_Id: item.exe_Id },
+          page: 1,
+          limit: 5,
+        });
+
+        // para cada transação aceita busca o tipo de transação
+        const tiposTransacao: TipoTransacao[] = [];
+        await Promise.all(
+          transacaoAceita.results.map(async (transacao) => {
+            const tipoTransacao = await this.TipoTransacaoRepository.findBy({
+              ttr_Id: transacao.ttr_Id,
+            });
+
+            if (!tipoTransacao) {
+              return;
+            }
+
+            tiposTransacao.push(tipoTransacao);
+          }),
+        );
+
+        Object.assign(item, {
+          livro,
+          categorias,
+          tiposTransacao,
         });
       }),
     );
